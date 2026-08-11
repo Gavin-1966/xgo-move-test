f51dfe85da4cfb79d897830fbc9e8e8f0afd3c8d
Gavin-1966 <gavs_mail@bigpond.com>
2026-08-12 06:59:23 +0800

    Basic Test

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+# Python3
+# Movement test of direction and speed
 
+# Movement functions
 
+def move_stop():
+    xgo.execution_action(xgo.action_enum.Default_posture)
+    return()
 
+def move_forward(spd=50): # input speed 
+    xgo.move_xgo(xgo.direction_enum.Forward, spd)
+    return()
+
+def move_back(spd=50): # input speed 
+    xgo.move_xgo(xgo.direction_enum.Backward, spd)
+    return()
+
+def turn_left(sec=2): # input time in seconds
+    xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_left, 60, sec)
+    return()
+
+def turn_right(sec=2): # input time in seconds
+    xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_right, 60, sec)
+    return()
+
+def timed_forward(spd=50, sec=2.5):
+    xgo.move_xgo(xgo.direction_enum.Forward, spd)
+    milisec=sec*1000
+    basic.pause(milisec)
+    return()
+
+def timed_back(spd=50, sec=2.5):
+    xgo.move_xgo(xgo.direction_enum.Backward, spd)
+    milisec=sec*1000
+    basic.pause(milisec)
+    return()
+
+# Initialise communication between Microbit and XGO
+xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
+xgo.execution_action(xgo.action_enum.Sit_down) # demonstrates connection sucessful
+
+while True:
+    # Test sequence on logo touch
+    if input.logo_is_pressed():
+        move_stop()
+        timed_forward()
+        timed_back()
+        turn_left()
+        turn_right()
+        move_stop()
+    
--- main.ts
+++ main.ts
@@ -1,1 +1,62 @@
+//  Python3
+//  Movement test of direction and speed
+//  Movement functions
+function move_stop(): any[] {
+    xgo.execution_action(xgo.action_enum.Default_posture)
+    return []
+}
 
+function move_forward(spd: number = 50): any[] {
+    //  input speed 
+    xgo.move_xgo(xgo.direction_enum.Forward, spd)
+    return []
+}
+
+function move_back(spd: number = 50): any[] {
+    //  input speed 
+    xgo.move_xgo(xgo.direction_enum.Backward, spd)
+    return []
+}
+
+function turn_left(sec: number = 2): any[] {
+    //  input time in seconds
+    xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_left, 60, sec)
+    return []
+}
+
+function turn_right(sec: number = 2): any[] {
+    //  input time in seconds
+    xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_right, 60, sec)
+    return []
+}
+
+function timed_forward(spd: number = 50, sec: number = 2.5): any[] {
+    xgo.move_xgo(xgo.direction_enum.Forward, spd)
+    let milisec = sec * 1000
+    basic.pause(milisec)
+    return []
+}
+
+function timed_back(spd: number = 50, sec: number = 2.5): any[] {
+    xgo.move_xgo(xgo.direction_enum.Backward, spd)
+    let milisec = sec * 1000
+    basic.pause(milisec)
+    return []
+}
+
+//  Initialise communication between Microbit and XGO
+xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
+xgo.execution_action(xgo.action_enum.Sit_down)
+//  demonstrates connection sucessful
+while (true) {
+    //  Test sequence on logo touch
+    if (input.logoIsPressed()) {
+        move_stop()
+        timed_forward()
+        timed_back()
+        turn_left()
+        turn_right()
+        move_stop()
+    }
+    
+}
